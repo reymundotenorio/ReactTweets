b8e6dd035020ac544833c3f398c1a84bb741272e
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-07-08 11:16:18 -0700

    Adding order function

--- tweets.js
+++ tweets.js
@@ -11,7 +11,7 @@ class Tweet extends React.Component {
         <p className="tweet-content">
           <b>{this.props.content}</b>
         </p>
-        <p className="tweet-date text-right">{moment(this.props.date).format("MMMM Do YYYY, h:mm:ss a")}</p>
+        <p className="tweet-date text-right">{moment(new Date(this.props.date)).format("MMMM Do YYYY, hh:mm:ss a")}</p>
         <p className="tweet-link">
           <a href={this.props.link} target="_blank">
             View tweet
@@ -388,34 +388,38 @@ class AppComponent extends React.Component {
       panels: []
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.orderUp = this.orderUp.bind(this);
+    this.orderDown = this.orderDown.bind(this);
   }
 
-  handleChange(event) {
-    // event.persist();
-    this.setState(prevState => ({
-      layout: prevState.layout.map(item =>
-        item.name === event.target.name ? { ...item, position: event.target.value } : item
-      )
-    }));
+  orderDown(name) {
+    console.log("Down ", name);
+    let newPosition = this.state.layout.find(item => item.name === name).position;
+    newPosition -= 1;
+
+    if (newPosition >= 0) {
+      this.setState(prevState => ({
+        layout: prevState.layout.map(item => (item.name === name ? { ...item, position: newPosition } : item))
+      }));
+
+      const column = name + "_column";
+      document.getElementById(column).style.order = newPosition;
+    }
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
+  orderUp(name) {
+    console.log("Up ", name);
+    let newPosition = this.state.layout.find(item => item.name === name).position;
+    newPosition += 1;
 
-    const formDOM = event.target;
-    const formValuesDOM = formDOM.elements;
+    if (newPosition <= 4) {
+      this.setState(prevState => ({
+        layout: prevState.layout.map(item => (item.name === name ? { ...item, position: newPosition } : item))
+      }));
 
-    this.setState(() => {
-      return { count: formValuesDOM.count.value };
-    });
-
-    this.getData();
-  }
-
-  toRight(event) {
-    console.log("AQUII", event);
+      const column = name + "_column";
+      document.getElementById(column).style.order = newPosition;
+    }
   }
 
   componentDidMount() {
@@ -431,27 +435,19 @@ class AppComponent extends React.Component {
           case "versa_agency":
             return (
               <React.Fragment key="versa_agency">
-                <div className="col-sm">
-                  <form className="form-inline sort-panel" onSubmit={this.handleSubmit}>
-                    <label htmlFor="count" className="mr-sm-2">
-                      Position:
-                    </label>
-                    <input
-                      type="number"
-                      className="form-control mb-2 mr-sm-2"
-                      name="versa_agency"
-                      min="1"
-                      max="3"
-                      value={this.state.layout.find(item => item.name === "versa_agency").position}
-                      onChange={event => {
-                        event.persist();
-                        this.handleChange(event);
-                      }}
-                    />
-                    <button type="submit" className="btn btn-primary mb-2">
-                      Sort
-                    </button>
-                  </form>
+                <div
+                  className="col-sm"
+                  id="versa_agency_column"
+                  style={{
+                    order: 1
+                  }}
+                >
+                  <button className="btn btn-dark" onClick={() => this.orderDown("versa_agency")}>
+                    -
+                  </button>
+                  <button className="btn btn-dark" onClick={() => this.orderUp("versa_agency")}>
+                    +
+                  </button>
 
                   <VersaTweets />
                 </div>
@@ -460,24 +456,19 @@ class AppComponent extends React.Component {
           case "rain_agency":
             return (
               <React.Fragment key="rain_agency">
-                <div className="col-sm">
-                  <form className="form-inline sort-panel" onSubmit={this.handleSubmit}>
-                    <label htmlFor="count" className="mr-sm-2">
-                      Position:
-                    </label>
-                    <input
-                      type="number"
-                      className="form-control mb-2 mr-sm-2"
-                      name="rain_agency"
-                      min="1"
-                      max="3"
-                      value={this.state.layout.find(item => item.name === "rain_agency").position}
-                      onChange={this.handleChange}
-                    />
-                    <button type="submit" className="btn btn-primary mb-2">
-                      Sort
-                    </button>
-                  </form>
+                <div
+                  className="col-sm"
+                  id="rain_agency_column"
+                  style={{
+                    order: 2
+                  }}
+                >
+                  <button className="btn btn-dark" onClick={() => this.orderDown("rain_agency")}>
+                    -
+                  </button>
+                  <button className="btn btn-dark" onClick={() => this.orderUp("rain_agency")}>
+                    +
+                  </button>
 
                   <RainAgencyTweets />
                 </div>
@@ -486,24 +477,19 @@ class AppComponent extends React.Component {
           case "alexa_devs":
             return (
               <React.Fragment key="alexa_devs">
-                <div className="col-sm">
-                  <form className="form-inline sort-panel" onSubmit={this.handleSubmit}>
-                    <label htmlFor="count" className="mr-sm-2">
-                      Position:
-                    </label>
-                    <input
-                      type="number"
-                      className="form-control mb-2 mr-sm-2"
-                      name="alexa_devs"
-                      min="1"
-                      max="3"
-                      value={this.state.layout.find(item => item.name === "alexa_devs").position}
-                      onChange={this.handleChange}
-                    />
-                    <button type="submit" className="btn btn-primary mb-2">
-                      Sort
-                    </button>
-                  </form>
+                <div
+                  className="col-sm"
+                  id="alexa_devs_column"
+                  style={{
+                    order: 3
+                  }}
+                >
+                  <button className="btn btn-dark" onClick={() => this.orderDown("alexa_devs")}>
+                    -
+                  </button>
+                  <button className="btn btn-dark" onClick={() => this.orderUp("alexa_devs")}>
+                    +
+                  </button>
 
                   <AlexaDevsTweets />
                 </div>
